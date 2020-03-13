625abd34408963a3fdd6dd61d028ad1e21e9c0f5
Richard Huang <flyerhzm@gmail.com>
2020-03-13 20:58:15 +0800

    do not transform multiline while to inline while

--- src/nodes/loops.js
+++ src/nodes/loops.js
@@ -3,69 +3,34 @@ const {
   concat,
   group,
   hardline,
-  ifBreak,
   indent,
-  softline
 } = require("../prettier");
-const { containsAssignment } = require("../utils");
 
-const printLoop = (keyword, modifier) => (path, { inlineLoops }, print) => {
+const printLoop = (keyword) => (path, { inlineLoops }, print) => {
+  const blockLoop = concat([
+    concat([
+      `${keyword} `,
+      align(keyword.length + 1, path.call(print, "body", 0))
+    ]),
+    indent(concat([hardline, path.call(print, "body", 1)])),
+    concat([hardline, "end"])
+  ]);
+
+  return blockLoop;
+};
+
+const printSingle = (keyword) => (path, { inlineLoops }, print) => {
   let inlineParts = [
     path.call(print, "body", 1),
     ` ${keyword} `,
     path.call(print, "body", 0)
   ];
 
-  // If the return value of this loop expression is being assigned to anything
-  // besides a local variable then we can't inline the entire expression
-  // without wrapping it in parentheses. This is because the following
-  // expressions have different semantic meaning:
-  //
-  //     hash[:key] = break :value while false
-  //     hash[:key] = while false do break :value end
-  //
-  // The first one will not result in an empty hash, whereas the second one
-  // will result in `{ key: nil }`. In this case what we need to do for the
-  // first expression to align is wrap it in parens, as in:
-  //
-  //     hash[:key] = (break :value while false)
   if (["assign", "massign"].includes(path.getParentNode().type)) {
     inlineParts = ["("].concat(inlineParts).concat(")");
   }
 
-  const inlineLoop = concat(inlineParts);
-
-  // If we're in the modifier form and we're modifying a `begin`, then this is a
-  // special case where we need to explicitly use the modifier form because
-  // otherwise the semantic meaning changes. This looks like:
-  //
-  //     begin
-  //       foo
-  //     end while bar
-  //
-  // The above is effectively a `do...while` loop (which we don't have in ruby).
-  if (modifier && path.getValue().body[1].type === "begin") {
-    return inlineLoop;
-  }
-
-  const blockLoop = concat([
-    concat([
-      `${keyword} `,
-      align(keyword.length + 1, path.call(print, "body", 0))
-    ]),
-    indent(concat([softline, path.call(print, "body", 1)])),
-    concat([softline, "end"])
-  ]);
-
-  // If we're disallowing inline loops or if the predicate of the loop contains
-  // an assignment (in which case we can't know for certain that that
-  // assignment doesn't impact the statements inside the loop) then we can't
-  // use the modifier form and we must use the block form.
-  if (!inlineLoops || containsAssignment(path.getValue().body[0])) {
-    return blockLoop;
-  }
-
-  return group(ifBreak(blockLoop, inlineLoop));
+  return concat(inlineParts);
 };
 
 // Technically this is incorrect. A `for` loop actually introduces and modifies
@@ -87,9 +52,9 @@ const printFor = (path, opts, print) =>
   );
 
 module.exports = {
-  while: printLoop("while", false),
-  while_mod: printLoop("while", true),
-  until: printLoop("until", false),
-  until_mod: printLoop("until", true),
+  while: printLoop("while"),
+  while_mod: printSingle("while"),
+  until: printLoop("until"),
+  until_mod: printSingle("until"),
   for: printFor
 };
--- test/js/loops.test.js
+++ test/js/loops.test.js
@@ -4,92 +4,59 @@ describe.each(["while", "until"])("%s", keyword => {
   test("aligns predicates", () =>
     expect(`foo ${keyword} ${long} || ${long}`).toChangeFormat(
       ruby(`
-      ${keyword} ${long} ||
-          ${Array(keyword.length)
-            .fill()
-            .join(" ")}${long}
-        foo
-      end
+      foo ${keyword} ${long} ||
+        ${long}
     `)
     ));
 
-  describe("inlines allowed", () => {
-    test("transforms to inline", () =>
-      expect(`${keyword} a\n  1\nend`).toChangeFormat(`1 ${keyword} a`));
+  test("transforms to inline", () =>
+    expect(`${keyword} a\n  1\nend`).toMatchFormat());
 
-    test("maintains inlines", () => expect(`1 ${keyword} a`).toMatchFormat());
+  test("maintains inlines", () => expect(`1 ${keyword} a`).toMatchFormat());
 
-    test("breaks on large predicates", () =>
-      expect(`${keyword} ${long}\n  1\nend`).toMatchFormat());
+  test("breaks on large predicates", () =>
+    expect(`${keyword} ${long}\n  1\nend`).toMatchFormat());
 
-    test("breaks inlines on large predicates", () =>
-      expect(`1 ${keyword} ${long}`).toChangeFormat(
-        `${keyword} ${long}\n  1\nend`
-      ));
+  test("breaks inlines on large predicates", () =>
+    expect(`1 ${keyword} ${long}`).toMatchFormat());
 
-    test("does not break into block when modifying a begin", () => {
-      const content = ruby(`
-        begin
-          foo
-        end ${keyword} bar
-      `);
+  test("does not break into block when modifying a begin", () => {
+    const content = ruby(`
+      begin
+        foo
+      end ${keyword} bar
+    `);
 
-      return expect(content).toMatchFormat();
-    });
-
-    test("breaks when an assignment is in the predicate", () => {
-      const content = ruby(`
-        ${keyword} (a = 1)
-          a
-        end
-      `);
-
-      return expect(content).toMatchFormat();
-    });
-
-    test("breaks when a multi assignment is in the predicate", () => {
-      const content = ruby(`
-        ${keyword} (a, b = 1, 2)
-          a
-        end
-      `);
-
-      return expect(content).toMatchFormat();
-    });
-
-    test("wraps single lines in parens when assigning", () =>
-      expect(
-        `hash[:key] = ${keyword} false do break :value end`
-      ).toChangeFormat(`hash[:key] = (break :value ${keyword} false)`));
+    return expect(content).toMatchFormat();
   });
 
-  describe("inlines not allowed", () => {
-    test("maintains multiline", () =>
-      expect(`${keyword} a\n  1\nend`).toMatchFormat({ inlineLoops: false }));
+  test("breaks when an assignment is in the predicate", () => {
+    const content = ruby(`
+      ${keyword} (a = 1)
+        a
+      end
+    `);
 
-    test("transforms to multiline", () =>
-      expect(`1 ${keyword} a`).toChangeFormat(`${keyword} a\n  1\nend`, {
-        inlineLoops: false
-      }));
-
-    test("breaks on large predicates", () =>
-      expect(`${keyword} ${long}\n  1\nend`).toMatchFormat({
-        inlineLoops: false
-      }));
-
-    test("breaks inlines on large predicates", () =>
-      expect(
-        `1 ${keyword} ${long}`
-      ).toChangeFormat(`${keyword} ${long}\n  1\nend`, { inlineLoops: false }));
-
-    test("does not break into block when modifying a begin", () => {
-      const content = ruby(`
-        begin
-          foo
-        end ${keyword} bar
-      `);
-
-      return expect(content).toMatchFormat({ inlineLoops: false });
-    });
+    return expect(content).toMatchFormat();
   });
+
+  test("breaks when a multi assignment is in the predicate", () => {
+    const content = ruby(`
+      ${keyword} (a, b = 1, 2)
+        a
+      end
+    `);
+
+    return expect(content).toMatchFormat();
+  });
+
+  test("wraps single lines in parens when assigning", () =>
+    expect(
+      `hash[:key] = ${keyword} false do break :value end`
+    ).toChangeFormat(ruby(`
+      hash[:key] =
+        ${keyword} false
+          break :value
+        end
+    `)));
 });
