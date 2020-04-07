3281fadd937fdaade5c4583639254c3d4911c420
Richard Huang <flyerhzm@gmail.com>
2020-04-07 19:29:52 +0800

    %r() => %r{}

--- src/nodes/regexp.js
+++ src/nodes/regexp.js
@@ -1,18 +1,16 @@
 const { concat } = require("../prettier");
 const { makeList } = require("../utils");
 
-const regexpPair = { ">": "<", ")": "(", "]": "[", "}": "{" };
-
 module.exports = {
   regexp: makeList,
   regexp_literal: (path, opts, print) => {
     const [contents, ending] = path.map(print, "body");
 
     const useSlash = ending[0] == "/";
-    const beginning = regexpPair[ending[0]] || ending[0];
-    const parts = [useSlash ? "/" : `%r${beginning}`]
-      .concat(contents)
-      .concat(ending);
+    const beginning = useSlash ? "/" : "%r{";
+    const newEnding =
+      (useSlash ? "/" : "}") + (ending.substring(1, ending.length) || "");
+    const parts = [beginning].concat(contents).concat(newEnding);
 
     return concat(parts);
   }
